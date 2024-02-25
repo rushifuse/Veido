16d2ce02061f7a69c79825935e271ac9287e578e
rushifuse <128386609+rushifuse@users.noreply.github.com>
2024-02-25 12:07:58 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function PlayVideo() {
+    var v = document.getElementsByTagName("video")[0];  
+    v.play(); 
+ }
+
+ function PauseViedo() {
+    var s = document.getElementsByTagName("video")[0];  
+    s.pause(); 
+ }
